d0fef8f9562f2852e6f11c3331503074c83b6d87
Daria <darialys2510@gmail.com>
2025-06-11 16:57:50 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', function (e) {
+  if (e.target.tagName !== 'IMG' && e.target.tagName !== 'A') {
+    return;
+  }
+
+  e.preventDefault();
+
+  const mainImage = document.getElementById('largeImg');
+
+  if (e.target.tagName === 'IMG') {
+    mainImage.src = e.target.parentElement.href;
+  } else if (e.target.tagName === 'A') {
+    mainImage.src = e.target.href;
+  }
+});
